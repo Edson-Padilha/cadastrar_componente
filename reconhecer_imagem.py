aa98d55a5adb6b726bf26116f56de96ef07fa67a
Edson-Padilha <edsonp.bsi@gmail.com>
2025-01-21 23:31:02 -0300

    Criando função para reconhecer imagens em arquivo separado

--- reconhecer_imagem.py
+++ reconhecer_imagem.py
@@ -1,1 +1,26 @@
-pass
+import pyautogui as pag
+from time import sleep
+
+cache_imagens = {}
+
+# Função para procurar imagens obrigatórias
+def localizar_imagem(caminho_imagem, confidence=0.9):
+    if caminho_imagem not in cache_imagens:
+        while True:
+            try:
+                localizacao = pag.locateOnScreen(caminho_imagem, confidence=confidence)
+
+                if localizacao is not None:
+                    print(f"Imagem {caminho_imagem} encontrada!")
+                    cache_imagens[caminho_imagem] = localizacao
+                    break
+
+                else:
+                    print(f"Imagem {caminho_imagem} não encontrada. Tentando naovamente...")
+                    sleep(1)
+
+            except pag.ImageNotFoundException:
+                print(f"Imagem {caminho_imagem} não encontrada. Tentando novamente...")
+                sleep(1)
+    
+    return cache_imagens[caminho_imagem]
